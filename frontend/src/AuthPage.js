9111fa170aa0c5061d4f1f750520534e7ef20d18
An0n1mity <monthouel@et.esiea.fr>
2023-02-10 20:36:58 +0200

    AuthPage

---
+++ frontend/src/AuthPage.js
@@ -0,0 +1,136 @@
+import { Component } from 'react';
+import axios from 'axios';
+
+class AuthPage extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      currentView: "signUp"
+    }
+  }
+
+  handleChange = (e) => {
+      this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { username, email, password } = this.state;
+    if (this.state.currentView === "signUp") {
+	axios.post('http://localhost:3001/auth/signup', {
+	    username: username,
+	    email: email,
+	    password: password,
+	    })
+	  .then(response => { console.log(response) })
+	  .catch(error => { console.log(error) });
+    }
+    else if (this.state.currentView === "logIn") {
+	axios.post('http://localhost:3001/auth/login', {
+	    username: username,
+	    password: password,
+	    })
+	  .then(response => { console.log(response) })
+	  .catch(error => { console.log(error) });
+	  }
+    }
+
+  changeView = (view) => {
+    this.setState({
+      currentView: view
+    })
+  }
+
+  currentView = () => {
+    switch(this.state.currentView) {
+      case "signUp":
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <h2>Sign Up!</h2>
+            <fieldset>
+              <legend>Create Account</legend>
+              <ul>
+                <li>
+                  <label for="username">Username:</label>
+                  <input type="text" id="username" onChange={this.handleChange} required/>
+                </li>
+                <li>
+                  <label for="email">Email:</label>
+                  <input type="email" id="email" onChange={this.handleChange} required/>
+                </li>
+                <li>
+                  <label for="password">Password:</label>
+                  <input type="password" id="password" onChange={this.handleChange} required/>              </li>
+              </ul>
+            </fieldset>
+            <button>Sign Up</button>
+            <button type="button" onClick={ () => this.changeView("logIn")}>Have an Account?</button>
+          </form>
+        )
+        break
+      case "logIn":
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <h2>Welcome Back!</h2>
+            <fieldset>
+              <legend>Log In</legend>
+              <ul>
+                <li>
+                  <label for="username">Username:</label>
+                  <input type="text" id="username" onChange={this.handleChange} required/>
+                </li>
+                <li>
+                  <label for="password">Password:</label>
+                  <input type="password" id="password" onChange={this.handleChange} required/>
+                </li>
+                <li>
+                  <i/>
+                  <a onClick={ () => this.changeView("PWReset")} href="#">Forgot Password?</a>
+                </li>
+              </ul>
+            </fieldset>
+            <button onClick={() => this.handleSubmit}>Submit</button>
+            <button type="button" onClick={ () => this.changeView("signUp")}>Create an Account</button>
+          </form>
+        )
+        break
+      case "PWReset":
+        return (
+          <form>
+          <h2>Reset Password</h2>
+          <fieldset>
+            <legend>Password Reset</legend>
+            <ul>
+              <li>
+                <em>A reset link will be sent to your inbox!</em>
+              </li>
+              <li>
+                <label for="email">Email:</label>
+                <input type="email" id="email" required/>
+              </li>
+            </ul>
+          </fieldset>
+          <button>Send Reset Link</button>
+          <button type="button" onClick={ () => this.changeView("logIn")}>Go Back</button>
+        </form>
+        )
+      default:
+        break
+    }
+  }
+
+
+  render() {
+    return (
+      <section id="entry-page">
+        {this.currentView()}
+      </section>
+    )
+  }
+}
+
+export default AuthPage
