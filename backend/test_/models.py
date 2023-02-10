48a6a5c6851ce43253bda004ea348c2c3dd8d9b1
An0n1mity <monthouel@et.esiea.fr>
2023-02-10 18:23:17 +0200

    game added to database

--- backend/test_/models.py
+++ backend/test_/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Game(models.Model):
+   white_player = models.ForeignKey(User, related_name='games_white_player')
+   black_player = models.ForeignKey(User, related_name='games_black_player')
+   current_turn = models.ForeignKey(User, related_name='games_current_turn')
+   game_state = models.TextField()
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+
 # Create your models here.
