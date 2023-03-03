a9861d25814e9134dd6b4c34609fc8c93c8532ac
An0n1mity <monthouel@et.esiea.fr>
2023-03-03 14:31:25 +0200

    added chessboard and online/bot button to frontpage

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -30,10 +30,57 @@ const Navbar = () => {
   );
 };
 
+const OnlineGameBlock = () => {
+  return (
+      <div className="online-game-block">
+	  <div className="online-game-block-board-img">
+	    <img className="board-img" src={require("./board.png")} alt="board" />
+	  </div>
+	  <div className="online-game-block-info">
+	    <h1 className="online-game-block-info-title">Play Chess <br/>Online !</h1>
+	    <div className="online-game-block-info-buttons">
+		<Link to="/play" style={{ textDecoration: 'none' }}>
+		    <button className="online-game-block-info-buttons-play-online">
+			<div className="online-game-block-info-buttons-play-online-img">
+			    <img className="online-play-button-img" src={require("./play-hand.png")} alt="play" />
+			</div>
+			<div className="online-game-block-info-buttons-play-online-text">
+			    <h1 className="online-game-block-info-buttons-play-online-text-title">Play Online</h1>
+			    <h2 className="online-game-block-info-buttons-play-online-text-subtitle">Play with your friends or random players</h2>
+			</div>
+		    </button>
+		</Link>
+		<Link to="/play" style={{ textDecoration: 'none' }}>
+		    <button className="online-game-block-info-buttons-play-bots">
+			<div className="online-game-block-info-buttons-play-bots-img">
+			    <img className="online-play-button-img" src={require("./play-hand.png")} alt="play" />
+			</div>
+			<div className="online-game-block-info-buttons-play-bots-text">
+			    <h1 className="online-game-block-info-buttons-play-bots-text-title">Play Bots</h1>
+			    <h2 className="online-game-block-info-buttons-play-bots-text-subtitle">Play with the computer</h2>
+			</div>
+		    </button>
+		</Link>
+		    
+	    </div>
+	  </div>
+      </div>
+  );
+};
+
+const HomeMainLayout = () => {
+  return (
+      <div className="home-main-layout">
+	<OnlineGameBlock />
+      </div>
+    );
+};
+
 const Home = () => {
   return (
       <div className="home">
 	<Navbar />
+	<HomeMainLayout />
       </div>
   );
 };
