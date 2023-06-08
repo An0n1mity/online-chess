c3f0806304d1e35baa8904ef3391b7fc0a064ffa
An0n1mity <monthouel@et.esiea.fr>
2023-06-08 18:04:15 +0300

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,6 +135,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # Replace with the actual origin of your React frontend
+]
+
 CSRF_TRUSTED_ORIGINS = [
         "http://localhost:3000",
         "http://127.0.0.1:3000",
