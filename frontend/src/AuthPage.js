5e93280914cae1e4d3ac2ee26f61f276496c2185
An0n1mity <monthouel@et.esiea.fr>
2023-06-08 22:44:48 +0300

    .

--- frontend/src/AuthPage.js
+++ frontend/src/AuthPage.js
@@ -96,7 +96,7 @@ class AuthPage extends Component {
 	    return
 	}
 	    
-      axios.post('http://localhost:8000/api/register', {
+      axios.post('http://localhost:80/api/register', {
 	    user:{
           username: username,
           email: email,
@@ -117,7 +117,7 @@ class AuthPage extends Component {
 	  });
     }
     else if (this.state.currentView === "logIn") {
-      axios.post('http://46.101.210.155:8000/api/login', {
+      axios.post('http://46.101.210.155:80/api/login', {
         user: {
           username: username,
           password: password
