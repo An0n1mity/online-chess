388a50dfc1b52648480cbd3dcde89020b8ee6477
An0n1mity <monthouel@et.esiea.fr>
2023-06-09 18:12:43 +0300

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,10 +55,13 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'test_',
     'rest_framework',
+    'rest_framework.authtoken',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
