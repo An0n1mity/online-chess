7836db5e9caf77ce4687df7448b7fe4048edade0
An0n1mity <monthouel@et.esiea.fr>
2023-06-10 12:44:28 +0300

    .

--- frontend/src/Url.js
+++ frontend/src/Url.js
@@ -1,3 +1,3 @@
-const backend_url = 'http://localhost:8000';
-//const backend_url = 'http://46.101.210.155:8000';
+//const backend_url = 'http://localhost:8000';
+const backend_url = 'http://46.101.210.155:8000';
 export { backend_url };
