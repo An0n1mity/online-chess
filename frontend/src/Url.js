026e892ec79f98cbd40c701aab132c5aff9d94f4
An0n1mity <monthouel@et.esiea.fr>
2023-06-12 14:57:13 +0000

    .

--- frontend/src/Url.js
+++ frontend/src/Url.js
@@ -1,3 +1,3 @@
-const backend_url = 'http://localhost:8000';
-//const backend_url = 'https://onlinechessproject.games:8000';
+//const backend_url = 'http://localhost:8000';
+const backend_url = 'https://onlinechessproject.games:8000';
 export { backend_url };
